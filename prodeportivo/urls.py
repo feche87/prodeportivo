cc41a977103458f89a934e58edc7bbdca2904e48
federico <federico.huel@gmail.com>
2014-04-06 00:38:54 -0300

    Commit inicial de prodeportivo. Contiene la estructura -carpetas y archivos de configuracion- del proyecto

---
+++ prodeportivo/urls.py
@@ -0,0 +1,26 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'prodeportivo.views.home', name='home'),
+    # url(r'^prodeportivo/', include('prodeportivo.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
+
+from prodeportivo import settings
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+            { 'document_root':settings.MEDIA_ROOT }
+        ),
+    )
